698a4a54e563f521bdc7502662553cbe4f3d5e2e
Aino Tani <bgg269@myy.haaga-helia.fi>
2020-05-10 12:04:03 +0300

    first commit

---
+++ muistikirja.py
@@ -0,0 +1,10 @@
+# -*- coding: cp1252 -*-
+# -*- coding: UTF8 -*-
+
+
+def main():
+    print("\n(1) Lue\n(2) Lis‰‰ merkint‰\n(3) Muokkaa merkint‰‰\n(4) Poista merkint‰\n(5) Tallenna ja lopeta")
+
+        
+if __name__ == "__main__":
+    main()
